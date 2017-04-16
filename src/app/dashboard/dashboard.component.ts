9cc91bfbaf72f59a264e9e5d5238784e6492f283
Vi Hoang <nguyenhoangvi000@gmail.com>
2017-04-16 13:10:13 +0700

    Update repo

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { MdSidenav } from '@angular/material';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
