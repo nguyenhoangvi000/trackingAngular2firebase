b58e40729a5a9bd249aa71770edc4eb457120b15
Vi Hoang <nguyenhoangvi000@gmail.com>
2017-05-04 00:12:45 +0700

    Update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
@@ -24,7 +24,7 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 
