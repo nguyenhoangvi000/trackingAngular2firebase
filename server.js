d2080f3e1d8fb66fc22f5a590abc0d77786ae583
Vi Hoang <nguyenhoangvi000@gmail.com>
2017-05-04 00:05:27 +0700

    Update

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
+app.use(express.static(__dirname + '/src'));
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
 const forceSSL = function() {
   return function (req, res, next) {
@@ -16,13 +21,10 @@ const forceSSL = function() {
   }
 }
 
+app.use(forceSSL());
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
-app.use(forceSSL());
 
-app.use(express.static(__dirname + '/src'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
