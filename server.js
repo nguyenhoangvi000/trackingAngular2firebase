918dcc854c3963c9b799d347368860f543ff7820
Vi Hoang <nguyenhoangvi000@gmail.com>
2017-05-03 23:38:12 +0700

    UPdate

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
+});
+
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/src'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
