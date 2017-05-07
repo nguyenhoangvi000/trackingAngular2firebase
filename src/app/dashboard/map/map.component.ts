32bcbde93cd6e304c9e419aed0ffaece3430d72d
Vi Hoang <nguyenhoangvi000@gmail.com>
2017-05-08 04:05:21 +0700

    Real time tracking

--- src/app/dashboard/map/map.component.ts
+++ src/app/dashboard/map/map.component.ts
@@ -27,11 +27,11 @@ export class MapComponent implements OnInit {
     // this.currentLat = currentPosition[0].lat;
     // this.currentLng = currentPosition[0].lng;
 
-    navigator.geolocation.watchPosition((position) => {
-      this.currentLat = position.coords.latitude;
-      this.currentLng = position.coords.longitude;
-      console.log(this.currentLat + ":" + this.currentLng);
-    })
+    // navigator.geolocation.watchPosition((position) => {
+    //   this.currentLat = position.coords.latitude;
+    //   this.currentLng = position.coords.longitude;
+    //   console.log(this.currentLat + ":" + this.currentLng);
+    // })
   }
 
   changeDriver(driver) {
@@ -40,11 +40,23 @@ export class MapComponent implements OnInit {
     this.geolocationCurrents = this.af.database.list(driverKey);
     console.log(driverKey);
 
-    navigator.geolocation.watchPosition((position) => {
-      this.currentLat = position.coords.latitude;
-      this.currentLng = position.coords.longitude;
-      console.log(this.currentLat + ":" + this.currentLng);
+    const positionCurrent = this.af.database.object(driverKey, { preserveSnapshot: true });
+    positionCurrent.subscribe(snapshot => {
+      let currentPos = [];
+      snapshot.forEach(element => {
+        currentPos.push(element);
+      });
+
+      this.currentLat = currentPos[currentPos.length - 1].val().lat;
+      this.currentLng = currentPos[currentPos.length - 1].val().lng;
+
     })
+
+    // navigator.geolocation.watchPosition((position) => {
+    //   this.currentLat = position.coords.latitude;
+    //   this.currentLng = position.coords.longitude;
+    //   console.log(this.currentLat + ":" + this.currentLng);
+    // })
 
   }
 
